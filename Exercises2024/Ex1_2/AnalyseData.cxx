e3cd8279c82a05ee36f9b088f712f67ace34f376
WAN AISHAH <165051742+Wanaiysha@users.noreply.github.com>
2024-11-18 02:16:15 +0000

    Calculate Resultant

---
+++ Exercises2024/Ex1_2/AnalyseData.cxx
@@ -0,0 +1,52 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream> 
+#include <utility> 
+#include "resultant.h"
+
+int main() {
+    std::string inputfile = "input2D_float.txt";
+    std::ifstream data_file(inputfile);
+    std::cout << "File opened successfully." << std::endl;
+    std::vector<std::pair<float, float>> data; // To store x, y pairs
+    std::string line;
+    float magnitude;
+    //Check file 
+    if (!data_file.is_open()) {
+        std::cout << "Error opening file: " << inputfile << std::endl;
+        return -1;
+    }
+     // Skip the first line (header)
+    if (!std::getline(data_file, line)) {
+        std::cout << "Error reading header or file is empty." << std::endl;
+        return -1;
+    }
+
+    // Process data lines
+    while (std::getline(data_file, line)) {
+        std::stringstream line_stream(line);
+        std::string x_string, y_string;
+
+        // Split by comma
+        if (std::getline(line_stream, x_string, ',') && std::getline(line_stream, y_string, ',')) {
+        
+                float x = std::stof(x_string); // Convert string to float
+                float y = std::stof(y_string);
+                data.emplace_back(x, y);   // Add to the vector
+             
+        } 
+    }
+
+    data_file.close();
+    // Output the data
+    std::cout << "Data read from file:" << std::endl;
+    for (const auto& [x_val, y_val] : data) {
+        std::cout << "x:y " << x_val << ":" << y_val << std::endl;
+    }
+    for (const auto& [x_val, y_val] : data) {
+        magnitude=resultantVector(x_val,y_val);
+        std::cout<< "Resultant="<<magnitude<<std::endl; 
+    }
+    return 0;
+}
---
+++ Exercises2024/Ex1_2/resultant.cxx
@@ -0,0 +1,10 @@
+// Wan Aishah Wan Harun
+// Preliminary Questions
+
+#include <iostream>
+#include <cmath> // Use cmath instead of math.h for C++
+
+// Function definition
+double resultantVector(double x_val, double y_val) {
+    return sqrt((x_val * x_val) + (y_val * y_val));
+}
